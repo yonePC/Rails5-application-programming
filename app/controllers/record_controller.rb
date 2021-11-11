8012f28d8c7dbf1aa20d57721ea0fbadfe36eab9
yonePC <ys.mailaddress0929@gmail.com>
2021-11-11 03:27:23 +0000

    5-2-4

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -2,28 +2,34 @@ class RecordController < ApplicationController
   def find
     @books = Book.find([2, 5, 10])
     render 'hello/list'
-  end  
-  
+  end
+
   def find_by
     @book = Book.find_by(publish: '技術評論社')
     render 'books/show'
   end
-  
+
   def find_by2
     @book = Book.find_by(publish: '技術評論社', price: 2980)
     render 'books/show'
   end
-  
+
   def where
     @books = Book.where(publish: '技術評論社')
     # @books = Book.where(published: '2016-06-01'..'2016-12-31')
     # @books = Book.where(publish: ['技術評論社', '翔泳社'])
     render 'hello/list'
   end
-  
+
+  # def ph1
+  #   @books = Book.where('publish = ? AND price >= ?',
+  #     params[:publish], params[:price])
+  #   render 'hello/list'
+  # end
+
   def ph1
-    @books = Book.where('publish = ? AND price >= ?',
-      params[:publish], params[:price])
+    @books = Book.where('publish = :publish AND price >= :price',
+      publish: params[:publish], price: params[:price])
     render 'hello/list'
   end
 end
