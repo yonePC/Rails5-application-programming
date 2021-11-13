bbecff083ee8aabdc732b58902abd80bb4e5c4e2
yonePC <ys.mailaddress0929@gmail.com>
2021-11-13 11:03:41 +0000

    5-2-6

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -37,4 +37,10 @@ class RecordController < ApplicationController
     @books = Book.where.not(isbn: params[:id])
     render 'books/index'
   end
+  
+  def where_or
+    @books = Book.where(publish: '技術評論社').or(Book.where('price > 2000'))
+    render 'hello/list'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -67,4 +67,5 @@ Rails.application.routes.draw do
   get 'record/keyword'
   post 'record/ph1'
   get 'record/not(/:id)' => 'record#not'
+  get 'record/where_or'
 end
