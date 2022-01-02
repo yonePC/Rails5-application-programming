4d1b210d48b8b0229c4f897ccdbfaabfd30f0592
yonePC <ys.mailaddress0929@gmail.com>
2022-01-02 02:30:06 +0000

    5-2-14

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -91,4 +91,9 @@ class RecordController < ApplicationController
   def groupby
     @books = Book.select('publish, AVG(price) AS avg_price').group(:publish)
   end
+  
+  def havingby
+    @books = Book.select('publish, AVG(price) AS avg_price').group(:publish).having('AVG(price) >= ?', 2500)
+    render 'record/groupby'
+  end
 end
